267b4ad8d1f60d9cba5893815315552d286c7b57
Ketan Maheshwari <km0@ornl.gov>
2021-01-01 15:49:23 -0500

    adding a couple files

---
+++ src/memavailable.sh
@@ -0,0 +1,6 @@
+for i in *.txt.gz
+do
+    # unique users:
+    memavailable=$(zcat $i | grep 'MemAvailable' | awk '{sum+=$2}END{print sum/(NR*1024*1024)}')
+    echo $i $memavailable >> dailyavgmemavailable.txt
+done
